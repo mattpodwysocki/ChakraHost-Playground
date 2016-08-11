1b6f4b845e899a4c4efb1cbe7fa8678f427026e2
mattpodwysocki <matthew.podwysocki@gmail.com>
2016-08-11 17:00:34 -0400

    Adding console methods

--- src/ChakraHost-Playground/ChakraHost.cpp
+++ src/ChakraHost-Playground/ChakraHost.cpp
@@ -1,10 +1,42 @@
 #include "stdafx.h"
 #include "ChakraHost.h"
 
+JsErrorCode DefineHostCallback(JsValueRef globalObject, const wchar_t *callbackName, JsNativeFunction callback, void *callbackState)
+{
+	JsPropertyIdRef propertyId;
+	IfFailRet(JsGetPropertyIdFromName(callbackName, &propertyId));
+
+	JsValueRef function;
+	IfFailRet(JsCreateFunction(callback, callbackState, &function));
+
+	IfFailRet(JsSetProperty(globalObject, propertyId, function, true));
+
+	return JsNoError;
+};
+
+JsValueRef CALLBACK ConsoleLog(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState)
+{
+	return JS_INVALID_REFERENCE;
+};
+
+JsValueRef CALLBACK ConsoleWarn(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState)
+{
+	return JS_INVALID_REFERENCE;
+};
+
+JsValueRef CALLBACK ConsoleInfo(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState)
+{
+	return JS_INVALID_REFERENCE;
+};
+
+JsValueRef CALLBACK ConsoleError(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState)
+{
+	return JS_INVALID_REFERENCE;
+};
+
 JsErrorCode ChakraHost::Init()
 {
 	currentSourceContext = 0;
-	JsErrorCode status = JsNoError;
 
 	IfFailRet(JsCreateRuntime(JsRuntimeAttributeNone, nullptr, &runtime));
 	IfFailRet(JsCreateContext(runtime, &context));
@@ -20,15 +52,18 @@ JsErrorCode ChakraHost::Init()
 	IfFailRet(JsCreateObject(&consoleObject));
 	IfFailRet(JsSetProperty(globalObject, consolePropertyId, consoleObject, true));
 
-	return status;
+	IfFailRet(DefineHostCallback(consoleObject, L"info", ConsoleInfo, nullptr));
+	IfFailRet(DefineHostCallback(consoleObject, L"log", ConsoleLog, nullptr));
+	IfFailRet(DefineHostCallback(consoleObject, L"warn", ConsoleWarn, nullptr));
+	IfFailRet(DefineHostCallback(consoleObject, L"error", ConsoleError, nullptr));
+
+	return JsNoError;
 };
 
 JsErrorCode ChakraHost::Destroy()
 {
-	JsErrorCode status = JsNoError;
-
 	IfFailRet(JsSetCurrentContext(JS_INVALID_REFERENCE));
 	IfFailRet(JsDisposeRuntime(runtime));
 
-	return status;
-}
+	return JsNoError;
+};
